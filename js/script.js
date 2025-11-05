6d84b7f4b61f0a9d4898079f75d9a4c12cc186ec
Cainã <caina.wanzeler@gmail.com>
2025-11-05 20:58:18 -0300

    update curriculo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 document.getElementById('year').innerText = new Date().getFullYear();
+
+const cards = document.querySelectorAll('.card-item');
+
+cards.forEach(card => {
+    card.addEventListener('mouseenter', function() {
+        this.style.backgroundColor =  '#eaeaea'
+    })
+    card.addEventListener('mouseleave', function() {
+        this.style.backgroundColor = 'var(--card)'
+    })
+})
